9dee17770a61a3761a723d13a5b4e273eca59547
Thebester <anthony.hoens@student.hepl.be>
2020-06-13 19:08:13 +0200

    Refresh the design

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
 const header = document.querySelector('header');
+const svgPath = document.querySelector('svg path');
+const li = document.querySelectorAll('nav li a');
+const divider = document.querySelector('.divider');
+console.log(divider.style)
 
 function scroll () {
+	if(window.innerWidth < 990) {
+		svgPath.style.fill = '#FFF';
+		divider.style.borderRightColor = '#FFF';
+		for(let i = 0; i < li.length; i++) {
+			li[i].style.color = '#FFF';
+		}
+	}
 	if(window.innerWidth > 990) {
+		svgPath.style.fill = '#000000';
+		divider.style.borderRightColor = '#000000';
+		for(let i = 0; i < li.length; i++) {
+			li[i].style.color = '#000000';
+		}
 		if(window.scrollY > 30) {
 			header.classList.add('scrollNavBar');
+			svgPath.style.fill = '#FFF';
+			divider.style.borderRightColor = '#FFF';
+			for(let i = 0; i < li.length; i++) {
+				li[i].style.color = '#FFF';
+			}
 		} else if (window.scrollY == 0) {
 			header.classList.remove('scrollNavBar');
+			svgPath.style.fill = '#000000';
+			divider.style.borderRightColor = '#000000';
+			for(let i = 0; i < li.length; i++) {
+				li[i].style.color = '#000000';
+			}
 		}
 	}
 	requestAnimationFrame(scroll);
